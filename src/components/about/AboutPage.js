87ce04bd6d898ab34051d608db6b85a62a555973
garymeehan <gary@donedeal.ie>
2017-07-27 20:32:34 +0100

    Initial App Structure

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class AboutPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This appilication uses React, Redux, React Router and a variety of other useful libaries</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
