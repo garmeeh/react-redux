e02f6a8554ab2c7c88b3a0667b0dbdf2cf966af0
garymeehan <gary@donedeal.ie>
2017-08-01 19:54:23 +0100

    Set up store and mock api

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,13 +1,47 @@
 import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
+  }
+
   render() {
+    const { courses } = this.props;
     return (
       <div>
         <h1>Courses Page</h1>
+        <CourseList courses={courses} />
       </div>
     );
   }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired,
+};
+
+function maptStateToProps(state, ownProps) {
+  return {
+    courses: state.courses,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+export default connect(
+  maptStateToProps,
+  mapDispatchToProps
+)(CoursesPage);
